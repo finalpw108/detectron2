22f6cda0fc4b7dec98e589f1ae83ecbcb6242852
JahnaviSatishShanbhag <71307702+JahnaviSatishShanbhag@users.noreply.github.com>
2023-02-04 12:48:40 +0530

    Update train_net.py

--- tools/train_net.py
+++ tools/train_net.py
@@ -150,9 +150,20 @@ def main(args):
         )
     return trainer.train()
 
+def train_multi_gpu():
+     args = default_argument_parser().parse_args()
+     launch(
+        main,
+        num_gpus_per_machine=2,
+        num_machines=1,
+        machine_rank=0,
+        dist_url="auto",
+        args=(args,),
+    )
+    
 
 if __name__ == "__main__":
-    args = default_argument_parser().parse_args()
+#     args = default_argument_parser().parse_args()
 #     print("Command Line Args:", args)
 #     launch(
 #         main,
@@ -162,11 +173,12 @@ if __name__ == "__main__":
 #         dist_url=args.dist_url,
 #         args=(args,),
 #     )
-    launch(
-        main,
-        num_gpus_per_machine=2,
-        num_machines=1,
-        machine_rank=0,
-        dist_url="auto",
-        args=(args,),
-    )
+#     launch(
+#         main,
+#         num_gpus_per_machine=2,
+#         num_machines=1,
+#         machine_rank=0,
+#         dist_url="auto",
+#         args=(args,),
+#     )
+    train_multi_gpu()
